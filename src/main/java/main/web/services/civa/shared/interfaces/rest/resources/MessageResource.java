95535d5412c7d1e5974a88bf4bd5679ca189fdc5
Fiorella <U202120418@upc.edu.pe>
2025-10-02 23:43:06 -0500

    first commit

---
+++ src/main/java/main/web/services/civa/shared/interfaces/rest/resources/MessageResource.java
@@ -0,0 +1,4 @@
+package com.acme.center.practica2pc2.shared.interfaces.rest.resources;
+
+public record MessageResource(String message) {
+}
---
+++ src/test/java/main/web/services/civa/CivaApplicationTests.java
@@ -0,0 +1,13 @@
+package main.web.services.civa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
